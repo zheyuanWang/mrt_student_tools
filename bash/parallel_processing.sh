af38f60760ef17a1086037917a43e5522dedc12e
zwang <zwang@mrt.kit.edu>
2020-08-17 10:33:55 +0200

    new: parallel processing

---
+++ bash/parallel_processing.sh
@@ -0,0 +1,17 @@
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+
+SRC_DICT="/mrtstorage/users/zwang/pcd_mapper_pastonly/polar_pastrange8/"
+#sequences=('00' '01' '02' '03' '04' '05' '06' '07' '08' '09' '10')
+#sequences=('02' '03' '04' '05')
+sequences=('02' '03' '04' '05')
+for SQ in "${sequences[@]}"
+do
+(
+echo "Processing SQ ${SQ}"
+python ./semanticVoxelPNG2Grid.py --input "${SRC_DICT}/${SQ}/fusion/maps" --colorized 1 --no_dyn_objs 1
+#python ./semanticVoxelPNG2Grid.py --input "${SRC_DICT}/${SQ}/single_shot/polar" --colorized 1
+) &
+done
+
+wait
